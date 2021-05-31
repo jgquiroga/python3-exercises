bd0c605abef7396823b547ebc3bcbb37688e188b
Julian Quiroga <jgquiroga@gmail.com>
2021-05-30 23:42:34 -0300

    Saving data to a json file

--- flask-exercises/ex-02-flask-pipenv/flask-pipenv-v3.py
+++ flask-exercises/ex-02-flask-pipenv/flask-pipenv-v3.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 app = Flask(__name__)
 
 # TODO: Add this information in a Readme file
@@ -25,6 +26,12 @@ def home():
 @app.route("/your-url", methods = ['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+
+        urls = {}
+        urls[request.form['code']] = { 'url': request.form['url'] }
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+
         # args is for querystring
         # form is for post
         return render_template('your_url.html', code = request.form['code'] )
