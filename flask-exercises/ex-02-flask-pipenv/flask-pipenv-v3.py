f0fa4c9baa5b16bfc376280748c89834ba312e92
Julian Quiroga <jgquiroga@gmail.com>
2021-05-31 00:10:07 -0300

    Adding file upload functionality and redirection

--- flask-exercises/ex-02-flask-pipenv/flask-pipenv-v3.py
+++ flask-exercises/ex-02-flask-pipenv/flask-pipenv-v3.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+
+# this is a random key
+app.secret_key = 'hafhadfhada343433ffd33f3f3fdfs'
 
 # TODO: Add this information in a Readme file
 # Prerequisites:
@@ -35,9 +40,19 @@ def your_url():
                 urls = json.load(urls_file)
 
         if request.form['code'] in urls.keys():
+            flash('That short name has already been taken. Please select another name.')
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = { 'url': request.form['url'] }
+        
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = { 'url': request.form['url'] }
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + '_' + secure_filename(f.filename)
+            f.save(os.path.join(app.root_path, 'uploads/' + full_name))
+            urls[request.form['code']] = { 'file': full_name }
+
+        
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
 
@@ -48,6 +63,14 @@ def your_url():
         # url_for receives the url of an action
         return redirect(url_for('home'))
 
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
 
 if __name__ == "__main__":
     app.run()
